e9d5cfdf07a0a44d1e7616ee5dd6da96f926c916
hvnahush <hvnahush@gmail.com>
2022-09-07 22:00:42 +0530

    updated

--- crypto-ui/src/app/coin-list/coin-list.component.ts
+++ crypto-ui/src/app/coin-list/coin-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-coin-list',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoinListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api : ApiService) { }
 
   ngOnInit(): void {
+    this.getbannerdata();
+    this.gettrendingdata();
   }
-
+  getbannerdata(){
+    this.api.getcurrency("INR").subscribe(res=>{
+      console.log(res);
+    })
+  }
+  gettrendingdata(){
+    this.api.gettrendingcurrency("INR").subscribe(res=>{
+      console.log(res);
+    })
+  }
 }
+
